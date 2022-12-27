a0c771af71b937cae73cf3b657b9d464f1b7ad14
Иван Верозуб <vanish.sun@gmail.com>
2022-12-27 19:18:30 +0500

    Добавлено отображение модели для админки

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'bio',
+        'role',
+        'confirmation_code',
+    )
+    search_fields = ('username', 'role',)
+    list_filter = ('username',)
+    empty_value_display = 'Empty'
