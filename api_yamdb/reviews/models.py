12a1223f72fb36de497068bec915aec38405e126
Руслан Мидехатович <shakertov@yandex.ru>
2022-12-27 20:42:51 +0500

    Add models.py to review app

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Title(models.Model):
+    """ Модель, определяющая произведения
+    """
+    name = models.CharField(max_length=255)
+    year = models.IntegerField()
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE
+    )
+
+
+class Category(models.Model):
+    """ Модель, определяющая категории
+    """
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+
+class Genre(models.Model):
+    """ Модель, определяющая жанры
+    """
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
