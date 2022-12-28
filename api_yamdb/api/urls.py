2ae5dcd8c165d93d196c6d5f93148871e54a721f
Иван Верозуб <vanish.sun@gmail.com>
2022-12-28 21:33:57 +0500

    fixed router import & add Views to signup and token pathes

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-from rest_framework import DefaultRouter
+from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet
+from .views import GetTokenView, SignUpView, UserViewSet
 
 app_name = 'api'
 
@@ -10,6 +10,6 @@ v1_router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
-    # path('v1/auth/signup/', __.as_view(), name='sign_up'),
-    # path('v1/auth/token/', __.as_view(), name='get_token'),
+    path('v1/auth/signup/', SignUpView.as_view(), name='sign_up'),
+    path('v1/auth/token/', GetTokenView.as_view(), name='get_token'),
 ]
