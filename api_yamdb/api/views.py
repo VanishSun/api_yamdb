e61d85f827034a3878dfeb35757f0b768af93fab
Иван Верозуб <vanish.sun@gmail.com>
2022-12-28 21:35:50 +0500

    created User, GetToken and SignUp views, but they are not done

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from users.models import User
+from .serializers import UserSerializer, GetTokenSerializer, SignUpSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class SignUpView(APIView):
+
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+
+class GetTokenView(APIView):
+
+    def post(self, request):
+        serializer = GetTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        user = get_object_or_404(User, username=data['username'])
+        if user.confirmation_code != data['confirmation_code']:
+            return Response(
+                {
+                    'confirmation_code': 'Код не верен!'
+                },
+                status=HTTP_400_BAD_REQUEST
+            )
+        return Response(
+            {
+                'token': str(
+                    RefreshToken.for_user(user).access_token
+                )
+            },
+            status=HTTP_201_CREATED
+        )
