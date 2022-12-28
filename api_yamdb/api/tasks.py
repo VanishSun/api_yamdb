11502792ebe00082ba61723995f737ee67db905c
Иван Верозуб <vanish.sun@gmail.com>
2022-12-28 22:35:07 +0500

    created tasks.py file for generate conf_code and send_msg

---
+++ api_yamdb/api/tasks.py
@@ -0,0 +1,11 @@
+def send_msg(email, name, subject, message):
+    # subject = f"Письмо от {name}"
+    # body = f""" {subject})
+    # Имя: {name}
+    # Адрес почты: {email}
+    # Тема: {subject}
+    # Сообдение: {message}
+    # """
+    # send_mail(
+    #     subject, body, email, ["vanish.sun@gmail.com"],
+    # )
