5ac96cf46a9af4781d4abd2c0c76d0e565c4e94d
Иван Верозуб <vanish.sun@gmail.com>
2022-12-29 18:14:29 +0500

    создал файл tasks.py с функцией отправки сообщения

--- api_yamdb/api/tasks.py
+++ api_yamdb/api/tasks.py
@@ -1,11 +1,16 @@
-def send_msg(email, name, subject, message):
-    # subject = f"Письмо от {name}"
-    # body = f""" {subject})
-    # Имя: {name}
-    # Адрес почты: {email}
-    # Тема: {subject}
-    # Сообдение: {message}
-    # """
-    # send_mail(
-    #     subject, body, email, ["vanish.sun@gmail.com"],
-    # )
+from django.core.mail import send_mail
+
+
+def send_msg(user):
+    subject = "Письмо с токеном"
+    email = user.email
+    body = f""" Привет, {user.username}!
+    Это письмо с кодом подтверждения для дальнейшего получения JWT токена.
+    Ниже твои данные:
+    Username: {user.username}
+    Адрес почты: {user.email}
+    Код подтверждения: {user.confirmation_code}
+    """
+    send_mail(
+        subject, body, email, ["vanish@gmail.com"],
+    )
