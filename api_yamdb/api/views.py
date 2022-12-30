ce15372aae5169a4f4f87e5baa24d546e765ff7a
Иван Верозуб <vanish.sun@gmail.com>
2022-12-30 16:22:52 +0500

    create action for Get&Patch metods in Me path

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,27 +1,53 @@
 from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
 from rest_framework.status import (
     HTTP_200_OK,
     HTTP_201_CREATED,
-    HTTP_400_BAD_REQUEST
+    HTTP_400_BAD_REQUEST,
 )
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from users.models import User
-from .serializers import GetTokenSerializer, SignUpSerializer, UserSerializer
+from .permissions import IsAdmin
+from .serializers import (
+    GetTokenSerializer,
+    SignUpSerializer,
+    UserProfileSerializer,
+    UserSerializer
+)
 from .tasks import send_msg
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, IsAdmin,)
     lookup_field = 'username'
     filter_backends = (filters.SearchFilter, )
     search_fields = ('username', )
+
+    @action(
+        methods=['GET', 'PATCH'],
+        detail=False,
+        permission_classes=(IsAuthenticated,),
+        url_path='me'
+    )
+    def get_current_user_info(self, request):
+        serializer = UserProfileSerializer(
+            request.user,
+            data=request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        if request.method == 'PATCH':
+            serializer.save()
+        return Response(serializer.data, status=HTTP_200_OK)
 
 
 class SignUpView(APIView):
