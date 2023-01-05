40fa70bb1dda04033e4fcb09d21385e8a503d484
Иван Верозуб <vanish.sun@gmail.com>
2023-01-05 22:20:49 +0500

    added third permission and updated second

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -16,13 +16,27 @@ class IsAdmin(permissions.BasePermission):
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
-
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return (
-                request.user.is_admin
-                or request.user.is_superuser
+        return (
+            request.method in permissions.SAFE_METHODS or (
+                request.user.is_authenticated and (
+                    request.user.is_admin or request.user.is_superuser
+                )
             )
-        return False
+        )
+
+
+class AutorAdminModeratorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_moderator
+            or request.user.is_admin
+        )
