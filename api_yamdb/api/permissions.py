005e9cf473858d7c4cbd3f0804d920310e5db9b5
Иван Верозуб <vanish.sun@gmail.com>
2023-01-08 17:00:11 +0500

    added is_superuser to permissions class

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,24 +8,17 @@ class IsAdmin(permissions.BasePermission):
             or request.user.is_admin
         )
 
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.user.is_superuser
-            or request.user.is_admin
-        )
-
 
 class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return (
-                request.user.is_admin
-                or request.user.is_superuser
+        return (
+            request.method in permissions.SAFE_METHODS or (
+                request.user.is_authenticated and (
+                    request.user.is_admin or request.user.is_superuser
+                )
             )
-        return False
+        )
 
 
 class IsAdminIsModeratorIsAuthor(permissions.BasePermission):
@@ -42,4 +35,5 @@ class IsAdminIsModeratorIsAuthor(permissions.BasePermission):
             or obj.author == request.user
             or request.user.is_admin
             or request.user.is_moderator
+            or request.user.is_superuser
         )
