c60fa7a86e5434cb4cfed0938fcf940e362169ba
Иван Верозуб <vanish.sun@gmail.com>
2023-01-08 18:27:35 +0500

    приемник post_save сигнала перенесен в models.py т.к. импорт в apps.py не проходил проверку flake8

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.tokens import default_token_generator
 from django.db.models import CharField, EmailField, TextField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from .validators import username_validator
 
@@ -83,3 +86,12 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'user'
         verbose_name_plural = 'users'
+
+
+@receiver(post_save, sender=User, dispatch_uid='create_confirmation_code')
+def create_confirmation_code(sender, instance, created, **kwargs):
+    if created:
+        confirmation_code = default_token_generator.make_token(instance)
+        instance.confirmation_code = confirmation_code
+        print(instance.confirmation_code)
+        instance.save()
