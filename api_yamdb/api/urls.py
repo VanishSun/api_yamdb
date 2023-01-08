4756aa787e65f0b43ebb9c19afd4f55451279e37
Иван Верозуб <vanish.sun@gmail.com>
2023-01-08 17:57:28 +0500

    отсортированы импорты

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -16,7 +16,7 @@ app_name = 'api'
 
 v1_router = DefaultRouter()
 
-v1_router.register('users', UserViewSet, basename='users')
+v1_router.register(r'users', UserViewSet, basename='users')
 v1_router.register(r'categories', CategoryViewSet, basename='categories')
 v1_router.register(r'genres', GenreViewSet, basename='genres')
 v1_router.register(r'titles', TitleViewSet, basename='titles')
