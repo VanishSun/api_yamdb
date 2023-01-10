4ff2c5b6df0bad66ca8b6960c786960a80107279
Valentina Koroleva <koroleva.valentina.a@gmail.com>
2023-01-10 21:45:39 +0200

    Учтены исправления по форматированию кода

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -171,19 +171,25 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAdminIsModeratorIsAuthor,
-                          permissions.IsAuthenticatedOrReadOnly)
+    permission_classes = (
+        IsAdminIsModeratorIsAuthor,
+        permissions.IsAuthenticatedOrReadOnly
+    )
 
     def get_queryset(self):
         "Возвращает комментарии к обьекту."
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
         review = get_object_or_404(
-            title.review, id=self.kwargs.get('review_id'))
+            title.review,
+            id=self.kwargs.get('review_id')
+        )
         return review.comments.all()
 
     def perform_create(self, serializer):
         "Создает комментарий от текущего пользователя."
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
         review = get_object_or_404(
-            title.review, id=self.kwargs.get('review_id'))
+            title.review,
+            id=self.kwargs.get('review_id')
+        )
         serializer.save(author=self.request.user, review=review)
